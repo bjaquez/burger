fc373bc5444e47b121c7c1997b7c751415da507b
Bianca Jaquez Chavez <biancajaquezchavez@Bianca-Jaquezs-MacBook-Pro.local>
2018-10-02 14:22:44 -0700

    removed connection

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "p1us8ottbqwio8hv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "t3hb7iyu94toqgmg",
-  password: "h6mfv32bnic52r3b",
+  user: "",
+  password: "",
   database: "u3s8i6hpvmserwb0"
 });
 
